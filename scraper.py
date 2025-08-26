53fe1b9478d4c7495c774d4eb2e68f000f6b4359
lostland <kimcs71@gmail.com>
2025-08-26 07:41:24 +0000

    test2

--- scraper.py
+++ scraper.py
@@ -23,13 +23,13 @@ def scrape_prices_simple(url, original_currency_code=None):
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--disable-gpu')
+        #chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--window-size=1028,720')  # 더 큰 화면
-        chrome_options.add_argument('--disable-logging')
-        chrome_options.add_argument('--log-level=3')
-        chrome_options.add_argument('--blink-setting=imagesEnable=false')
+        #chrome_options.add_argument('--disable-logging')
+        #chrome_options.add_argument('--log-level=3')
+        #chrome_options.add_argument('--blink-setting=imagesEnable=false')
         #chrome_options.page_load_strategy = 'eager' # 또는 'none'으로 변경 가능
-        chrome_options.add_argument('--disable-extensions')
+        #chrome_options.add_argument('--disable-extensions')
         # 실제 브라우저처럼 보이게 하는 옵션들
         chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
         #chrome_options.add_argument('--accept-language=en-US,en;q=0.9')
@@ -75,7 +75,7 @@ def scrape_prices_simple(url, original_currency_code=None):
             f.flush()
 
             #driver.set_script_timeout(5)
-            driver.set_page_load_timeout(20)
+            driver.set_page_load_timeout(10)
             driver.get(url)
             f.write(f"finish driver.get(): {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
             f.flush()
